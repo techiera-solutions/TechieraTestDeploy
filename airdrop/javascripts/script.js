91cecd5965e7d48759ffbed8e07b7dd21454ffb1
Aditi Hooda <aditi.hooda@gmail.com>
2024-07-21 12:41:59 +0530

    deploying airdrop

--- airdrop/javascripts/script.js
+++
@@ -1,8 +0,0 @@
-console.log(
-    '%cWe are hiring!',
-    'color: #f709bb; font-family: sans-serif; text-decoration: underline;'
-);
-console.groupCollapsed('Contact');
-console.log('Rishabh Gupta');
-console.log('mailto:rishi.dipsite@gmail.com');
-console.groupEnd();
